9c6da001fd949b868d7dcca9e3eb25e849dc4adc
Michelle Casey <mcasey5216>
2015-10-24 14:26:23 -0400

    fixed numbered order of rankings

--- js/main.js
+++ js/main.js
@@ -15,22 +15,36 @@ $("#add").click(function(){
   } else {
     nameScoreHash[nameScoreArray[0]] += parseInt(nameScoreArray[1]);
   }
+  $("input").val('');
+  scoreboardSort();
+});
+
+$("#clear").click(function(){
+  nameScoreHash = {};
+  $("#rankings").empty();
+  $("input").val('');
+});
+
+var scoreboardSort = function() {
   var scoreboard = [];
   for (var name in nameScoreHash) {
     scoreboard.push([name, nameScoreHash[name]]);
     scoreboard.sort(function(a, b) {return b[1] - a[1]});
   }
   $("#rankings").empty();
+  var rank = 0;
+  var lastScore = 0;
+  var rankSkip = 0;
   for (i = 0; i < scoreboard.length; i++) {
+    if (scoreboard[i][1] !== lastScore) {
+      rank = rank + rankSkip + 1;
+      rankSkip = 0;
+    } else {
+      rankSkip += 1;
+    }
+    lastScore = scoreboard[i][1];
     $("#rankings").append(
-      "<li>"+ scoreboard[i][0] + ", " + scoreboard[i][1] + " pts</li>"
+      "<li>"+ rank + ". " + scoreboard[i][0] + ", " + scoreboard[i][1] + " pts</li>"
     );
   }
-});
-
-$("#clear").click(function(){
-  nameScoreHash = {};
-  $("#rankings").empty();
-});
-
-var scoreboardSort
+}
